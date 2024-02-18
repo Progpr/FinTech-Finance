cef5000145d6c36994e34d8a8e1158016fd3c1f2
Atharva Patil <95381092+Progpr@users.noreply.github.com>
2024-02-18 10:54:02 +0000

    dashboard changes #3

--- dashboard2.py
+++ dashboard2.py
@@ -4,7 +4,8 @@ import numpy as np
 import yfinance as yf 
 import plotly.express as px
 
-st.title("Stock Dashboard")
+st.title("FinTech Dashboard")
+st.subheader("Research about your desired stock by simply typing its stock symbol")
 ticker = st.sidebar.text_input("Ticker")
 start_date = st.sidebar.date_input("Start date")
 end_date = st.sidebar.date_input("End date")
@@ -21,7 +22,7 @@ else:
         fig = px.line(data, x = data.index, y = data['Adj Close'], title = ticker)
         st.plotly_chart(fig)
     except:
-        st.write("Please select a more older start date")
+        st.subheader("Please select a more older start date")
 
 
     #different tabs
@@ -29,16 +30,47 @@ else:
     overview_data, fund_data, news_data = st.tabs(["Overview", "Fundamentals", "News"])
 
     with overview_data:
+        #contents here:
+        #company title
+        #description
+        #industry
+        #country
+        #ceo - name and title
+        #company website
+
         info = yf.Ticker(ticker).info
 
         st.subheader(info["longName"])
-
+        
+        st.subheader("About")
         st.write(info["longBusinessSummary"])
+        
+        st.subheader("Industry")
+        st.write(info["industryDisp"])
+
+        st.subheader("Location")
+        st.write(info["city"])
+        st.write(info["country"])
+        
+        officers = info['companyOfficers']
+        ceo = officers[0]
+        st.subheader(ceo["title"])
+        st.write(ceo["name"])
 
     with fund_data:
         st.write("Fundamentals")
         st.write(info)
 
     with news_data:
-        st.write("News")
+        news = yf.Ticker(ticker).news
+
+        for items in news:
+            st.subheader(items["title"])
+            st.write(items["publisher"])
+            st.write(items["link"])
+            st.write("Related tickers")
+            for ticker in items["relatedTickers"]:
+                st.write(ticker)
+        
+
 
