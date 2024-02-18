431a5bc1992ca5ba1672284b4ea6e58066ffb4ce
Atharva Patil <95381092+Progpr@users.noreply.github.com>
2024-02-18 09:29:53 +0000

    dashboard changes #2

--- dashboard2.py
+++ dashboard2.py
@@ -9,28 +9,36 @@ ticker = st.sidebar.text_input("Ticker")
 start_date = st.sidebar.date_input("Start date")
 end_date = st.sidebar.date_input("End date")
 
-# getting data from yahoo finance
-data = yf.download(ticker, start = start_date, end=end_date)
 
-# plotting data
-fig = px.line(data, x = data.index, y = data['Adj Close'], title = ticker)
-st.plotly_chart(fig)
+if not ticker:
+    st.subheader("Please enter a ticker")
+else:
+    try:
+        # getting data from yahoo finance
+        data = yf.download(ticker, start = start_date, end=end_date)
+
+        # plotting data
+        fig = px.line(data, x = data.index, y = data['Adj Close'], title = ticker)
+        st.plotly_chart(fig)
+    except:
+        st.write("Please select a more older start date")
 
 
-#different tabs
-#overview, fundamentals and news
-overview_data, fund_data, news_data = st.tabs(["Overview", "Fundamentals", "News"])
+    #different tabs
+    #overview, fundamentals and news
+    overview_data, fund_data, news_data = st.tabs(["Overview", "Fundamentals", "News"])
 
-with overview_data:
-    st.write(f"About {ticker}")
+    with overview_data:
+        info = yf.Ticker(ticker).info
 
-    infos = yf.Ticker(ticker).info
+        st.subheader(info["longName"])
 
-    st.write(infos["longBusinessSummary"])
+        st.write(info["longBusinessSummary"])
 
-with fund_data:
-    st.write("Fundamentals")
+    with fund_data:
+        st.write("Fundamentals")
+        st.write(info)
 
-with news_data:
-    st.write("News")
+    with news_data:
+        st.write("News")
 
